532dd7010fe02d96bf3daabbcb2e16136e94f387
Jessica Kerr <jessitron@gmail.com>
2019-03-07 14:18:40 -0600

    Update SpringRestSeedController.java

--- src/main/java/com/atomist/springteam/springrestseed/SpringRestSeedController.java
+++ src/main/java/com/atomist/springteam/springrestseed/SpringRestSeedController.java
@@ -30,7 +30,7 @@ class SpringRestSeedController {
 
 	@RequestMapping(method = GET, path = "/")
 	public String root() {
-		return "Spring Boot REST service running in Legacy Park " + getClass().getName();
+		return "Spring Boot REST service running at home " + getClass().getName();
 	}
 
 	@RequestMapping(method = GET, path = "hello/{name}")
